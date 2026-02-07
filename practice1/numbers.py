7532329c513d135ecd7f7f52485b1f8f10ea11f2
Alina <alinasakenkyzy@gmail.com>
2026-02-07 11:46:14 +0500

    Add practice1 structure

---
+++ practice1/numbers.py
@@ -0,0 +1,6 @@
+x = 18
+y = 3.5
+z = 4j
+print(type(x))
+print(type(y))
+print(type(z))
---
+++ practice1/string.py
@@ -0,0 +1,19 @@
+# slicing strings
+a = "PYTHON"
+print (a[0:3])
+
+#Modify Strings
+a = "What are,you doing?"
+print(a.upper())
+print(a.lower())
+print(a.replace("W", "D"))
+print(a.split(","))
+
+#String Concatenation
+a = "My name is"
+b = "Alina"
+c = a + " " + b
+print(c)
+
+
+
