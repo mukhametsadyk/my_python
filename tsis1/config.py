c518aed78bb9425471f4f6ac6d47a9c0159d8ef7
Alina <alinasakenkyzy@gmail.com>
2026-05-02 12:20:24 +0500

    remove tsis1 only

--- tsis1/config.py
+++
@@ -1,4 +0,0 @@
-host = "localhost"
-database = "phonebook_db01"
-user = "postgres"
-password = "enhypen301120"
--- tsis1/connect.py
+++
@@ -1,5 +0,0 @@
-import psycopg2
-from config import DB_CONFIG
-
-def get_conn():
-    return psycopg2.connect(**DB_CONFIG)
